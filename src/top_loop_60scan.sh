d19966b8600ba5d75176f48d8acd0c42f341b503
Ilya Bezrukov <rbetra@gmail.com>
2014-02-04 16:47:28 +0400

    edit top -> top -HSz

--- src/top_loop_60scan.sh
+++ src/top_loop_60scan.sh
@@ -2,4 +2,4 @@
 TOP_LOG=$1
 NUM=$2
 END=$(( NUM * 60/3 ))
-for (( i=1; i<=$END; i++ )); do top -b -Ha -n 20 >> ${TOP_LOG}; sleep 3; done &
+for (( i=1; i<=$END; i++ )); do top -b -HSz -n 20 >> ${TOP_LOG}; sleep 3; done &
