8a3bdc8068df2560c7d29485162151e8424f93c3
Ilya Bezrukov <rbetra@gmail.com>
2014-02-10 13:16:15 +0400

    change run script for dd ssd test

--- test_dd/SSD/3ssd-stripe.sh
+++ test_dd/SSD/3ssd-stripe.sh
@@ -14,5 +14,5 @@ DISK_TYPE="ssd"
 
 BASE_PATH="`pwd`/src"
 
-#Start dd_tester.sh
-/bin/bash ${BASE_PATH}/dd_tester.sh ${NUM} ${POOL_DISK} ${POOL_TYPE} ${DISK_TYPE} ${BASE_PATH}
+#Start dd_test.sh
+/bin/bash ${BASE_PATH}/dd_test.sh ${NUM} ${POOL_DISK} ${POOL_TYPE} ${DISK_TYPE} ${BASE_PATH}
