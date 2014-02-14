666b5767ed818e1f96b64a48d1abf3b826fcc44f
Ilya <rbetra@gmail.com>
2014-02-15 00:53:53 +0400

    8pool iostat

--- src/ziostat.sh
+++ src/ziostat.sh
@@ -4,7 +4,11 @@ NUM=$2
 
 SEC=$((NUM*24))
 
-for i in {0..7}
-do
-    /sbin/zpool iostat storage${i} 10 ${SEC} > ${LOG}_ch${i}
-done
+/sbin/zpool iostat storage0 10 ${SEC} > ${LOG}_ch0 &
+/sbin/zpool iostat storage1 10 ${SEC} > ${LOG}_ch1 &
+/sbin/zpool iostat storage2 10 ${SEC} > ${LOG}_ch2 &
+/sbin/zpool iostat storage3 10 ${SEC} > ${LOG}_ch3 &
+/sbin/zpool iostat storage4 10 ${SEC} > ${LOG}_ch4 &
+/sbin/zpool iostat storage5 10 ${SEC} > ${LOG}_ch5 &
+/sbin/zpool iostat storage6 10 ${SEC} > ${LOG}_ch6 &
+/sbin/zpool iostat storage7 10 ${SEC} > ${LOG}_ch7
