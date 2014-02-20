ee17178c8074f517f3b65a7cb0ee84d1204cd628
Ilya Bezrukov <rbetra@gmail.com>
2014-02-20 13:28:28 +0400

    edit dd_tester with 'wait'

--- src/dd_tester.sh
+++ src/dd_tester.sh
@@ -7,7 +7,7 @@ RAND=/old/root/rand.data.0
 for i in 0 1 2; do dd if=${RAND} of=/dev/null bs=128K; done
 
 END=$(( 100+${NUM} ))
-for ind in {0..END}
+for ((ind=1; ind<=$END; ind++))
 do
     echo "Step $ind"
     dd if=${RAND} of=/ch0/test_dd_${ind} bs=1m >> ${LOG}_ch0 2>&1 &
