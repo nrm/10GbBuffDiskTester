735bec5922c9e49746acafce627f33b04be14a7b
Ilya <rbetra@gmail.com>
2014-02-20 23:29:46 +0400

    edit zfs revers gpt labels

--- zfs/sas/2dstripe.sh
+++ zfs/sas/2dstripe.sh
@@ -10,16 +10,42 @@ do
     /sbin/zpool destroy storage$i
 done
 
-# create zpool STRIPE 3 disk
-/sbin/zpool create storage0 /dev/gpt/md2d0 /dev/gpt/md2d1
-/sbin/zpool create storage1 /dev/gpt/md2d2 /dev/gpt/md2d3
-/sbin/zpool create storage2 /dev/gpt/md2d4 /dev/gpt/md2d5
-/sbin/zpool create storage3 /dev/gpt/md2d6 /dev/gpt/md2d7
+#MD2
+#md2d0
+#md2d1
+#md2d2
+#md2d3
+#md2d4
+#md2d5
+#md2d6
+#md2d7
+#md2d8
+#md2d9
+#md2d10
+#md2d11
+#md2d12
+#md2d13
+#md2d14
+#md2d15
+#md2d16
+#md2d17
+#md2d18
+#md2d19
+#md2d20
+#md2d21
+#md2d22
+#md2d23
 
-/sbin/zpool create storage4 /dev/gpt/md1d0 /dev/gpt/md1d1
-/sbin/zpool create storage5 /dev/gpt/md1d2 /dev/gpt/md1d3
-/sbin/zpool create storage6 /dev/gpt/md1d4 /dev/gpt/md1d5
-/sbin/zpool create storage7 /dev/gpt/md1d6 /dev/gpt/md1d7
+# create zpool STRIPE 3 disk
+/sbin/zpool create storage0 /dev/gpt/md2d23 /dev/gpt/md2d22
+/sbin/zpool create storage1 /dev/gpt/md2d21 /dev/gpt/md2d20
+/sbin/zpool create storage2 /dev/gpt/md2d19 /dev/gpt/md2d18
+/sbin/zpool create storage3 /dev/gpt/md2d17 /dev/gpt/md2d16
+
+/sbin/zpool create storage4 /dev/gpt/md1d23 /dev/gpt/md1d22
+/sbin/zpool create storage5 /dev/gpt/md1d21 /dev/gpt/md1d20
+/sbin/zpool create storage6 /dev/gpt/md1d19 /dev/gpt/md1d18
+/sbin/zpool create storage7 /dev/gpt/md1d17 /dev/gpt/md1d16
 
 # create mountpoint
 for i in {0..7}
--- zfs/sas/3draidz.sh
+++ zfs/sas/3draidz.sh
@@ -11,15 +11,15 @@ do
 done
 
 # create zpool RAIDZ 3 disk
-/sbin/zpool create storage0 raidz /dev/gpt/md2d0 /dev/gpt/md2d1 /dev/gpt/md2d2
-/sbin/zpool create storage1 raidz  /dev/gpt/md2d3 /dev/gpt/md2d4 /dev/gpt/md2d5
-/sbin/zpool create storage2 raidz /dev/gpt/md2d6 /dev/gpt/md2d7 /dev/gpt/md2d8
-/sbin/zpool create storage3 raidz /dev/gpt/md2d9 /dev/gpt/md2d10 /dev/gpt/md2d11
+/sbin/zpool create storage0 raidz /dev/gpt/md2d23 /dev/gpt/md2d22 /dev/gpt/md2d21
+/sbin/zpool create storage1 raidz /dev/gpt/md2d20 /dev/gpt/md2d19 /dev/gpt/md2d18
+/sbin/zpool create storage2 raidz /dev/gpt/md2d17 /dev/gpt/md2d16 /dev/gpt/md2d15
+/sbin/zpool create storage3 raidz /dev/gpt/md2d14 /dev/gpt/md2d13 /dev/gpt/md2d12
 
-/sbin/zpool create storage4 raidz /dev/gpt/md1d0 /dev/gpt/md1d1 /dev/gpt/md1d2
-/sbin/zpool create storage5 raidz /dev/gpt/md1d3 /dev/gpt/md1d4 /dev/gpt/md1d5
-/sbin/zpool create storage6 raidz /dev/gpt/md1d6 /dev/gpt/md1d7 /dev/gpt/md1d8
-/sbin/zpool create storage7 raidz /dev/gpt/md1d9 /dev/gpt/md1d10 /dev/gpt/md1d11
+/sbin/zpool create storage4 raidz /dev/gpt/md1d23 /dev/gpt/md1d22 /dev/gpt/md1d21
+/sbin/zpool create storage5 raidz /dev/gpt/md1d20 /dev/gpt/md1d19 /dev/gpt/md1d18
+/sbin/zpool create storage6 raidz /dev/gpt/md1d17 /dev/gpt/md1d16 /dev/gpt/md1d15
+/sbin/zpool create storage7 raidz /dev/gpt/md1d14 /dev/gpt/md1d13 /dev/gpt/md1d12
 
 # create mountpoint
 for i in {0..7}
