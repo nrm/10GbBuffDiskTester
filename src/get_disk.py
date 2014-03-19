9d0cef06337e55e4d7864101e50ba9e415f9d99d
Ilya Bezrukov <rbetra@gmail.com>
2014-03-19 17:30:15 +0400

    add get da and gpt

---
+++ src/get_disk.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Соотносим номер устройства /dev/da* и gpt label
+
+"""
+import subprocess
+import traceback
+import re
+import sys
+
+
+def get_da_str(string):
+    """
+    Get drive address from string"""
+    p=re.compile(r'da\d+')
+    try:
+        s=p.search(string)
+        return string[s.start():s.end()]
+    except AttributeError:
+        raise NameError("Empty string")
+
+
+def get_all_da():
+    """
+    Get all disk address"""
+    try:
+        P=subprocess.check_output(["camcontrol", "dev"])
+    except Exception:
+        print traceback.format_exc()
+        sys.exit()
+
+    result_da=[]
+    for line in P.split("\n"):
+        try:
+            result_da.append(get_da_str(line))
+        except NameError:
+            pass
+    return result_da
+
+
+def get_gpt(disknumber):
+    """
+    Get GPT labels from gpart
+    """
+    try:
+        P=subprocess.check_output(["gpart", "show", "-l", "%s"%disknumber])
+    except Exception:
+        print traceback.format_exc()
+        sys.exit()
+    #dict_gpt-da={} # {"gpt-label":"disk number"}
+    return P.strip().split()[-2]
+
+
+def compare_disk_gpt():
+    """docstring for compare_disk_gpt"""
+    #get all disk drive in camcontrol
+    all_da=get_all_da()
+    gpt_da=dict()
+    for disk in all_da:
+        gpt_da["%s"%get_gpt(disk)]=disk
+
+    return gpt_da
+
+
+def args():
+    """
+    Запуск программы
+        ключи:
+            -i, --infile: infile log file
+            -o, --outmode: output mode ["K", "M", "B", "G"]
+    """
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Программа управления ШСПС')
+    parser.add_argument("-i", "--infile", metavar="infile",
+                      help="infile log file")
+    parser.add_argument("-o", "--outmode", metavar="outmode",
+                      help='output mode ["K", "M", "B", "G"]')
+
+
+    args = parser.parse_args()
+    if not args.infile:
+        print parser.print_help()
+    else:
+        if not args.infile:
+            output="M"
+        else:
+            output=args.outmode
+        pass
+        #result = get_speed_one_log(infile=args.infile, outmode=output)
+        #print "%.3f\t%.3f\t%f"%(result[0], result[1], result[2])
+
+
+if __name__ == "__main__":
+    #args()
+    print compare_disk_gpt()
+
--- zfs/nsas/2dstripe.sh
+++ zfs/nsas/2dstripe.sh
@@ -11,15 +11,15 @@ do
 done
 
 # create zpool STRIPE 3 disk
-/sbin/zpool create storage0 /dev/gpt/disk0 /dev/gpt/disk1
-/sbin/zpool create storage1 /dev/gpt/disk2 /dev/gpt/disk3
-/sbin/zpool create storage2 /dev/gpt/disk4 /dev/gpt/disk5
-/sbin/zpool create storage3 /dev/gpt/disk6 /dev/gpt/disk7
+/sbin/zpool create storage0 /dev/gpt/disk23 /dev/gpt/disk22
+/sbin/zpool create storage1 /dev/gpt/disk21 /dev/gpt/disk20
+/sbin/zpool create storage2 /dev/gpt/disk19 /dev/gpt/disk18
+/sbin/zpool create storage3 /dev/gpt/disk16 /dev/gpt/disk17
 
-/sbin/zpool create storage4 /dev/gpt/disk8 /dev/gpt/disk9
-/sbin/zpool create storage5 /dev/gpt/disk10 /dev/gpt/disk11
-/sbin/zpool create storage6 /dev/gpt/disk12 /dev/gpt/disk13
-/sbin/zpool create storage7 /dev/gpt/disk14 /dev/gpt/disk15
+/sbin/zpool create storage4 /dev/gpt/disk16 /dev/gpt/disk15
+/sbin/zpool create storage5 /dev/gpt/disk13 /dev/gpt/disk14
+/sbin/zpool create storage6 /dev/gpt/disk12 /dev/gpt/disk11
+/sbin/zpool create storage7 /dev/gpt/disk9 /dev/gpt/disk10
 
 # create mountpoint
 for i in {0..7}
